bf659d16b1eacb0b8b96e9c7f06b238c88831cc1
mads <mads@mads-ThinkPad-T14s-Gen-2i.SkumHus>
2023-07-28 10:17:49 +0200

    feat: adding packages

--- install.sh
+++ install.sh
@@ -1,25 +1,57 @@
+#!/bin/bash
+# Install file for setting up my enviroment.
 
-# Install packages
-#nix-env -iA \
-#	nixpkgs.zsh \
-#	nixpkgs.git \
-#	nixpkgs.neovim \
-#	nixpkgs.tmux \
-#	nixpkgs.stow \
-#	nixpkgs.fzf \
-#	nixpkgs.ripgrep \
-#	nixpkgs.bat \
-#	nixpkgs.direnv \
-#	nixpkgs.zoxide \
-#	nixpkgs.starship \
-#	nixpkgs.lsd \
-#	nixpkgs.zig \
-#	nixpkgs.fd
+# Install misc packages
+sudo dnf -y install mpv
+sudo dnf -y install shellcheck
+sudo dnf -y install yamllint
+sudo dnf -y install pylint
+
+nix-env -iA \
+	nixpkgs.tealdeer \ # better tldr is used to get short informaiton about a program
+	nixpkgs.volumeicon \ # applet with support for global keybindings
+	nixpkgs.zsh \ # better shell
+	nixpkgs.git \ # Versioning system
+	nixpkgs.neovim \ # Text editor
+	nixpkgs.tmux \ # console multiplexing
+	nixpkgs.stow \ # symlink farm manager
+	nixpkgs.skim \ # fuzzy search im RUST
+	nixpkgs.ripgrep \ # recursive regex search
+	nixpkgs.bat \ # cat but smarter
+	nixpkgs.direnv \ # set up different variables based on folder
+	nixpkgs.zoxide \ # cd alternative
+	nixpkgs.exa \ # ls alternative
+	nixpkgs.fd \ # find alternative
+	nixpkgs.prettyping \ # Pretty formatted ping
+	nixpkgs.figlet \ # Text to ascii art
+	nixpkgs.htop \ # better top
+	nixpkgs.neofetch \ # system information
+	nixpkgs.onefetch \ # Neofetch but for git
+	nixpkgs.speedtest-cli \ # speedtest cli tool
+	nixpkgs.calcurse \ # cli calendar
+	nixpkgs.duf \ # Disk usage and info
+	nixpkgs.docker \
+	nixpkgs.docker-compose \
+	nixpkgs.feh \ # lightweight image viewer
+	nixpkgs.deno \ # javascript and typescript runtime
+	nixpkgs.tfswitch \ # switch between terraform versions
+	nixpkgs. \
+
+# Install virtualization packages
+dnf -y install qemu-kvm
+dnf -y install iptables
+dnf -y install dmidecode
+dnf -y install virt-manager
+dnf -y install virt-viewer
+dnf -y install dnsmasq
+nix-env -iA \
+	nixpkgs.vde2 \
+	nixpkgs.bridge-utils \
+	nixpkgs.netcat-openbsd \
+	nixpkgs.ebtables \
 
 # Install oneoff apps
 curl -L https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh | bash
-
-#testtest
 
 # set up zplug
 mkdir -p /usr/share/zsh/scripts/zplug
