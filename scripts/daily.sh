200ab9b803f49472954a5152299f299b29f6c1cc
Mads Kristiansen <mads@plikki.com>
2024-03-18 20:15:53 +0100

    refactor

--- scripts/daily.sh
+++ scripts/daily.sh
@@ -3,52 +3,54 @@
 # Source other bash scripts
 . ./log.sh
 
+# Running examples:
+# source daily
+
 # Setting variables
 today=$(date +"%Y%m%d")
 dailypath="$SECOND_BRAIN/dailynotes"
 
-# A bash script to create daily notes
-# What i need this script to do:
-# - Run with no arguments
-# - take todays day and see if file already exists
-#   - If not exists, create file and drop me into editor (Nvim)
-#   - If file exist, crop me into editor (nvim)
-# - If new file is created, copy todo's from last daily note into new one.
-# Running examples:
-# daily
-
-
+# Function to fetch the newest daily file
 get_newest_daily(){
-    elog "Fetching newest daily file"
-    edebug "loading array into filearray"
+    # Create array of files from daily path
+    edebug "loading array from $dailypath"
     filearray=($("ls" -r $dailypath))
+
+    # Put the newest daily file into a variable
     edebug "defineing newest_daily to be: ${filearray[1]}"
     newest_daily=${filearray[1]}
     edebug "newest_daily: $newest_daily"
 }
 
+# Function that opens or creates todays daily file, this would also create the
+# file if its missing
 open_file() {
-  if [ -e "$dailypath/$today.md" ]; then
-      edebug "open's op today.md"
-      nvim "$dailypath/$today.md"
-  else
-      edebug "start function to get newest daily"
-      get_newest_daily
-      # Sets up timestamp to use later
-      timestamp=$(date +"%Y-%m-%d")
-     
-      edebug "cd into dailypath"
-      # CD into daily path
-      cd "$dailypath" || exit
-      edebug "current directory $(pwd)"
+    if [ -e "$dailypath/$today.md" ]; then
+        # CD Into dailypath directory
+        cd "$dailypath" || exit
+
+        # Opens daily file in Neovim
+        edebug "open's op today.md"
+        nvim "$dailypath/$today.md"
+    else
+        # Runs function that finds the newest daily file
+        edebug "start function to get newest daily"
+        get_newest_daily
+
+        # Sets up timestamp to use later
+        timestamp=$(date +"%Y-%m-%d")
+
+        # CD into daily path
+        edebug "cd into dailypath"
+        cd "$dailypath" || exit
       
-      edebug "Create $today.md file"
-      # Create daily file
-      touch "$today.md"
+        # Create daily file
+        edebug "Create $today.md file"
+        touch "$today.md"
       
-      edebug "fill out $today.md with default infomation"
-      # input all content into today that is needed
-      tee "$today.md" << EOF
+        # input all content into today that is needed
+        edebug "fill out $today.md with default infomation"
+        tee "$today.md" << EOF
 ---
 date: $(date +"%Y-%m-%d")
 tag:
@@ -58,19 +60,23 @@ tag:
 ## Agenda
 
 ## Todo
+$(grep -Pi '^-\s\[(?!x\])(.)\]' $newest_daily)
 
 ## Notes
-$(grep -Pi '^-\s\[(?!x\])(.)\]' $newest_daily)
 
 ## links
 EOF
-    edebug "fix all open tasks in newest_daily"
-    sed -i 's/\[ \]/\[X\]/g' $newest_daily
+        # Remove old TODO's
+        edebug "fix all open tasks in newest_daily"
+        sed -i 's/\[ \]/\[X\]/g' $newest_daily
 
-    edebug "open up $today.md with neovim"
-    nvim "$today.md"
+        # Opens up todays note in Neovim
+        edebug "open up $today.md with neovim"
+        nvim "$today.md"
 
-    fi
+        fi
 }
+
+# Run openfile function
 edebug "trying to run open_file function"
 open_file
