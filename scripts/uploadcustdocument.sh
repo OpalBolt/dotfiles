64f2ebb85d4ee01078a5ca439f0c0caaa5962fa1
Mads Kristiansen <mads@plikki.com>
2024-08-12 19:31:46 +0200

    fix: update scripts related to operations in git folders

--- scripts/uploadcustdocument.sh
+++ scripts/uploadcustdocument.sh
@@ -4,5 +4,4 @@ cd $CUSTOMERS_PATH
 pwd
 git add .
 git commit -m "Update"
-rclone -v copy ~/git/customers gdrive:Customers --exclude=/.git/ --exclude=/.obsidian/
-
+rclone -v copy ~/git/work/customers gdrive:Customers --exclude=/.git/ --exclude=/.obsidian/
--- scripts/waylandscreen.sh
+++ scripts/waylandscreen.sh
@@ -1,3 +1,4 @@
-wlr-randr --output eDP-1 --scale 2
+#wlr-randr --output eDP-1 --scale 2
 wlr-randr --output HDMI-A-1 --pos 0,0
-wlr-randr --output eDP-1 --pos 320,1080
+#wlr-randr --output eDP-1 --pos 320,1080
+wlr-randr --output eDP-1 --off
