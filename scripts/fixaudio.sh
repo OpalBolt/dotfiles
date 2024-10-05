6da7a4120386e308a946f88336a4d3d2b02d277f
Mads Kristiansen <mads@plikki.com>
2024-10-05 14:41:36 +0200

    fix(script): fix audio to kill pipewire

--- scripts/fixaudio.sh
+++ scripts/fixaudio.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+pkill -9 pipewire-pulse && pkill -9 pipewire
+sleep 1
 systemctl --user restart pipewire-pulse.socket
 systemctl --user restart pipewire-pulse.service
 systemctl --user restart pipewire.service
